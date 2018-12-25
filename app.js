f41353ef798cc4ead9daaaac58f40c90b34f98f2
Nico De Witte <nico.dewitte@outlook.com>
2018-12-25 09:37:34 +0100

    Connect to the two brokers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
+const mqtt = require('mqtt');
 
 let config = null;
 try {
@@ -9,4 +10,20 @@ try {
   process.exit();
 }
 
-console.log(config);
+let source  = mqtt.connect(config.source.connection, {
+  username: config.source.username,
+  password: config.source.password
+});
+
+let destination  = mqtt.connect(config.destination.connection, {
+  username: config.destination.username,
+  password: config.destination.password
+});
+
+source.on('connect', function () {
+  console.log("Successfully connected to source broker");
+});
+
+destination.on('connect', function () {
+  console.log("Successfully connected to destination broker");
+});
