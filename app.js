c8b12e11551c222cc887964c6100cdd61a1e0590
Nico De Witte <nico.dewitte@outlook.com>
2018-12-25 09:23:57 +0100

    Load yaml config file

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Requires implementation");
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+let config = null;
+try {
+  config = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'));
+} catch (e) {
+  console.log("Failed to load configuration file (config.yaml). Did you make one?");
+  process.exit();
+}
+
+console.log(config);
